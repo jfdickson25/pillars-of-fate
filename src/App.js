425501596fdff3058498308e7db26d6cdfc8d380
Jordan Dickson <jfdickson25@gmail.com>
2025-11-09 12:48:31 -0700

    Add God selection at beginning of game

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import { ReactComponent as FeatherIcon } from './Feather.svg';
 import { ReactComponent as ScorpionIcon } from './Scorpion.svg';
 export default function App() {
 
+    const [activeGods, setActiveGods] = useState([]);
+    const [godConfirmed, setGodConfirmed] = useState(false);
+
     const [cards, setCards] = useState([
         { id: 1, value: '', column: 1, row: 1, feathers: [false, false, false, false, false, false], scorpions: [false, false, false, false, false, false] },
         { id: 2, value: '', column: 2, row: 1, feathers: [false, false, false, false, false, false], scorpions: [false, false, false, false, false, false] },
@@ -43,6 +46,16 @@ export default function App() {
         }
 
         setCards(newCards);
+    };
+
+    const setGods = (godName) => {
+        if (activeGods.includes(godName)) {
+            setActiveGods(activeGods.filter(name => name !== godName));
+        } else {
+            if(activeGods.length < 3) {
+                setActiveGods([...activeGods, godName]);
+            }
+        }
     };
 
     return (
@@ -117,6 +130,33 @@ export default function App() {
                 ])} } 
                 />
             </div>
+            {   !godConfirmed &&
+                <div id="god-icons-container">
+                    <div id="god-icons">
+                        <img src={`${process.env.PUBLIC_URL}/images/God-Icons/Agamar-icon.PNG`} alt="Agamar" className={activeGods.some(name => name === "Agamar") ? "god-icon active" : "god-icon"} onClick={() => {setGods("Agamar")}}/>
+                        <img src={`${process.env.PUBLIC_URL}/images/God-Icons/Aponi-icon.PNG`} alt="Aponi" className={activeGods.some(name => name === "Aponi") ? "god-icon active" : "god-icon"} onClick={() => {setGods("Aponi")}}/>
+                        <img src={`${process.env.PUBLIC_URL}/images/God-Icons/Arbiter-icon.PNG`} alt="" className={activeGods.some(name => name === "Arbiter") ? "god-icon active" : "god-icon"} onClick={() => {setGods("Arbiter")}}/>
+                        <img src={`${process.env.PUBLIC_URL}/images/God-Icons/Belan-icon.PNG`} alt="" className={activeGods.some(name => name === "Belan") ? "god-icon active" : "god-icon"} onClick={() => {setGods("Belan")}}/>
+                        <img src={`${process.env.PUBLIC_URL}/images/God-Icons/Eradan-icon.PNG`} alt="" className={activeGods.some(name => name === "Eradan") ? "god-icon active" : "god-icon"} onClick={() => {setGods("Eradan")}}/>
+                        <img src={`${process.env.PUBLIC_URL}/images/God-Icons/Hadria-icon.PNG`} alt="" className={activeGods.some(name => name === "Hadria") ? "god-icon active" : "god-icon"} onClick={() => {setGods("Hadria")}}/>
+                        <img src={`${process.env.PUBLIC_URL}/images/God-Icons/Invictus-icon.PNG`} alt="" className={activeGods.some(name => name === "Invictus") ? "god-icon active" : "god-icon"} onClick={() => {setGods("Invictus")}}/>
+                        <img src={`${process.env.PUBLIC_URL}/images/God-Icons/Isabel-icon.PNG`} alt="" className={activeGods.some(name => name === "Isabel") ? "god-icon active" : "god-icon"} onClick={() => {setGods("Isabel")}}/>
+                        <img src={`${process.env.PUBLIC_URL}/images/God-Icons/Klar-icon.PNG`} alt="" className={activeGods.some(name => name === "Klar") ? "god-icon active" : "god-icon"} onClick={() => {setGods("Klar")}}/>
+                        <img src={`${process.env.PUBLIC_URL}/images/God-Icons/Mother-to-All-icon.PNG`} alt="" className={activeGods.some(name => name === "Mother-to-All") ? "god-icon active" : "god-icon"} onClick={() => {setGods("Mother-to-All")}}/>
+                        <img src={`${process.env.PUBLIC_URL}/images/God-Icons/Naka-icon.PNG`} alt="" className={activeGods.some(name => name === "Naka") ? "god-icon active" : "god-icon"} onClick={() => {setGods("Naka")}}/>
+                        <img src={`${process.env.PUBLIC_URL}/images/God-Icons/Namari-icon.PNG`} alt="" className={activeGods.some(name => name === "Namari") ? "god-icon active" : "god-icon"} onClick={() => {setGods("Namari")}}/>
+                        <img src={`${process.env.PUBLIC_URL}/images/God-Icons/Penance-icon.PNG`} alt="" className={activeGods.some(name => name === "Penance") ? "god-icon active" : "god-icon"} onClick={() => {setGods("Penance")}}/>
+                        <img src={`${process.env.PUBLIC_URL}/images/God-Icons/Pentha-icon.PNG`} alt="" className={activeGods.some(name => name === "Pentha") ? "god-icon active" : "god-icon"} onClick={() => {setGods("Pentha")}}/>
+                        <img src={`${process.env.PUBLIC_URL}/images/God-Icons/Prophet-icon.PNG`} alt="" className={activeGods.some(name => name === "Prophet") ? "god-icon active" : "god-icon"} onClick={() => {setGods("Prophet")}}/>
+                        <img src={`${process.env.PUBLIC_URL}/images/God-Icons/Saghari-icon.PNG`} alt="" className={activeGods.some(name => name === "Saghari") ? "god-icon active" : "god-icon"} onClick={() => {setGods("Saghari")}}/>
+                        <img src={`${process.env.PUBLIC_URL}/images/God-Icons/Sorcerer-icon.PNG`} alt="" className={activeGods.some(name => name === "Sorcerer") ? "god-icon active" : "god-icon"} onClick={() => {setGods("Sorcerer")}}/>
+                        <img src={`${process.env.PUBLIC_URL}/images/God-Icons/Steward-icon.PNG`} alt="" className={activeGods.some(name => name === "Steward") ? "god-icon active" : "god-icon"} onClick={() => {setGods("Steward")}}/>
+                        <img src={`${process.env.PUBLIC_URL}/images/God-Icons/Tormentor-icon.PNG`} alt="" className={activeGods.some(name => name === "Tormentor") ? "god-icon active" : "god-icon"} onClick={() => {setGods("Tormentor")}}/>
+                        <img src={`${process.env.PUBLIC_URL}/images/God-Icons/Vesper-icon.PNG`} alt="" className={activeGods.some(name => name === "Vesper") ? "god-icon active" : "god-icon"} onClick={() => {setGods("Vesper")}}/>
+                        <button id="confirm-gods-button" onClick={ () => { setGodConfirmed(true); } } disabled={ activeGods.length !== 3}>CONFIRM</button>
+                    </div>
+                </div>
+            }
         </React.Fragment>
     );
 }
